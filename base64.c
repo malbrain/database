03438c536b835e7418612aa85d0d1100942f6996
Karl Malbrain <malbrain@yahoo.com>
2019-02-22 08:12:51 -0800

    add base64 to project

---
+++ base64.c
@@ -0,0 +1,77 @@
+#include <stdint.h>
+#include <time.h>
+
+const char* base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+long mynrand48(unsigned short xseed[3]);
+ 
+int createB64(char *key, int size, unsigned short next[3])
+{
+uint64_t byte8;
+int base;
+
+	for( base = 0; base < size; base++ ) {
+		if( base % 8 == 0 ) {
+			byte8 = (uint64_t)mynrand48(next) << 32;
+			byte8 |= mynrand48(next);
+		}
+		key[base] = base64[byte8 & 0x3f];
+		byte8 >>= 6;
+	}         	
+
+	return base;
+}
+
+//	implement reentrant nrand48
+
+#define RAND48_SEED_0   (0x330e)
+#define RAND48_SEED_1   (0xabcd)
+#define RAND48_SEED_2   (0x1234)
+#define RAND48_MULT_0   (0xe66d)
+#define RAND48_MULT_1   (0xdeec)
+#define RAND48_MULT_2   (0x0005)
+#define RAND48_ADD      (0x000b)
+
+unsigned short _rand48_add = RAND48_ADD;
+unsigned short _rand48_seed[3] = {
+    RAND48_SEED_0,
+    RAND48_SEED_1,
+    RAND48_SEED_2
+};
+
+unsigned short _rand48_mult[3] = {
+    RAND48_MULT_0,
+    RAND48_MULT_1,
+    RAND48_MULT_2
+};
+
+void mynrand48seed(uint16_t *nrandState) {
+time_t tod[1];
+
+	time(tod);
+	nrandState[0] = RAND48_SEED_0 ^ *tod;
+	*tod >>= 16;
+	nrandState[1] = RAND48_SEED_1 ^ *tod;
+	*tod >>= 16;
+	nrandState[2] = RAND48_SEED_2 ^ *tod;
+}
+
+long mynrand48(unsigned short xseed[3]) 
+{
+unsigned short temp[2];
+unsigned long accu;
+
+    accu = (unsigned long)_rand48_mult[0] * (unsigned long)xseed[0] + (unsigned long)_rand48_add;
+    temp[0] = (unsigned short)accu;        /* lower 16 bits */
+    accu >>= sizeof(unsigned short) * 8;
+    accu += (unsigned long)_rand48_mult[0] * (unsigned long)xseed[1]; 
+	accu += (unsigned long)_rand48_mult[1] * (unsigned long)xseed[0];
+    temp[1] = (unsigned short)accu;        /* middle 16 bits */
+    accu >>= sizeof(unsigned short) * 8;
+    accu += _rand48_mult[0] * xseed[2] + _rand48_mult[1] * xseed[1] + _rand48_mult[2] * xseed[0];
+    xseed[0] = temp[0];
+    xseed[1] = temp[1];
+    xseed[2] = (unsigned short)accu;
+    return ((long)xseed[2] << 15) + ((long)xseed[1] >> 1);
+}
+
